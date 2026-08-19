3484df67a26f2826ee2651bbda9cf40672c2b48a
ian <ianlynxk@gmail.com>
2026-08-19 15:02:16 +0800

    ansible: zsh app + systemic include tag propagation via apply (task 4)

---
+++ playbooks/roles/software/files/apps/zsh/.config/zsh/conf.d/90-catppuccin-highlight.zsh
@@ -0,0 +1,3 @@
+# catppuccin mocha palette for zsh-syntax-highlighting (owned by: zsh app)
+[[ -f ~/.local/share/catppuccin_mocha-zsh-syntax-highlighting.zsh ]] && \
+  source ~/.local/share/catppuccin_mocha-zsh-syntax-highlighting.zsh
---
+++ playbooks/roles/software/files/apps/zsh/.config/zsh/conf.d/99-syntax-highlighting.zsh
@@ -0,0 +1,3 @@
+# syntax highlighting must be sourced last (owned by: zsh app)
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main)
